571fe20315c3f28f45b0d8961470752691fd0fa1
anis rbh <mohamedrbh519@gmail.com>
2022-05-12 14:23:29 +0100

    4 - useEffect (part 2)

--- App.js
+++ App.js
@@ -1,37 +1,76 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Home from './components/Home';
 import axios from 'axios';
 
-export default function App() {
+// export default function App() {
+//   const [users, setUsers] = useState([]);
+
+//   useEffect(() => {
+//     // Faire des appels API
+//     // https://api.github.com/users
+//     console.log('useEffect App');
+//     // Promise
+//     axios.get('https://api.github.com/users').then(response => {
+//       console.log('REPONSE data : ', response.data);
+//       setUsers(response.data);
 
 
-  useEffect(() => {
-    // Faire des appels API
-    // https://api.github.com/users
-    console.log('useEffect App');
-    // Promise
-    axios.get('https://api.github.com/users').then(response => {
-      console.log('REPONSE : ', response);
-    }).catch(error => {
-      console.log('error', error);
-    });
-  }, []);
+//     }).catch(error => {
+//       console.log('error', error);
+//     });
+//   }, []);
+//   // [{id: ..., login : ...}, {id: ..., login : ...} ....]
+//   const userListJsx = users.map((element) => {
+//       return (
+//       <View key={element.id} style={styles.user}>
+//         <Text>ID : {element.id}</Text>
+//         <Text>Username : {element.login}</Text>
+//         <Image source={{uri: element.avatar_url}} style={styles.image} />
+//       </View>
+//       )
+//   })
 
-  console.log('return App');
+//   return (
+//     <View style={styles.container}>
+//       {userListJsx}
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   user:{
+//     backgroundColor:'lightblue',
+//     borderRadius: 20,
+//     padding: 10,
+//     margin: 20,
+//     alignItems:'center'
+
+//   },
+//   image:{
+//     width:100,
+//     height: 100
+//   }
+// });
+
+
+
+
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Home />
-      {/* <Image source={{uri: ...}} */}
+    <View>
+      <Text>App</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+const styles = StyleSheet.create({})
